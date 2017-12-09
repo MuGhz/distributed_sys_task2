d04391821ed47235e11d74d9734e8c88a831d421
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-12-09 21:12:44 +0700

    add get saldo and transfer

--- c-ewallet.py
+++ c-ewallet.py
@@ -11,7 +11,7 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 channel.exchange_declare(exchange='EX_REGISTER', exchange_type='direct',durable=True)
-result = channel.queue_declare(exclusive=True)
+result = channel.queue_declare()
 queue_name = result.method.queue
 channel.queue_bind(exchange='EX_REGISTER',queue=queue_name,routing_key='REQ_1406559055')
 print ('[X] Waiting for logs')
@@ -51,5 +51,105 @@ def request_register(ch, method, properties, body):
 		print ("[E] Error :",e)
 
 channel.basic_consume(request_register, queue=queue_name, no_ack=True)
+
+channel.exchange_declare(exchange='EX_TRANSFER', exchange_type='direct',durable=True)
+result = channel.queue_declare()
+queue_name = result.method.queue
+channel.queue_bind(exchange='EX_TRANSFER',queue=queue_name,routing_key='REQ_1406559055')
+def transfer(msg):
+	database.connect()
+	resp = {}
+	resp['action'] = 'transfer'
+	resp['type'] = 'response'
+	resp['ts']= '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+	try :
+		user_id = msg['user_id']
+		sender_id = msg['sender_id']
+		nilai = msg['nilai']
+	except Exception as e:
+		print ("[E] Error :",e)
+		resp['status_transfer'] = -99
+		resp = json.dumps(resp)
+		return channel.basic_publish(exchange='EX_TRANSFER',routing_key='RESP_'+sender_id,body=resp)
+	try :
+		user = User.get(npm=user_id)
+	except Exception as e:
+		print ("[E] Error :",e)
+		resp['status_transfer'] = -1
+		resp = json.dumps(resp)
+		return channel.basic_publish(exchange='EX_TRANSFER',routing_key='RESP_'+sender_id,body=resp)
+	if nilai < 0 or nilai > 1000000000 :
+		print ("[E] Error :",e)
+		resp['status_transfer'] = -5
+		resp = json.dumps(resp)
+		return channel.basic_publish(exchange='EX_TRANSFER',routing_key='RESP_'+sender_id,body=resp)
+	try :
+		user.saldo += nilai
+		user.save()
+	except Exception as e :
+		print ("[E] Error :",e)
+		resp['status_transfer'] = -4
+		resp = json.dumps(resp)
+		return channel.basic_publish(exchange='EX_TRANSFER',routing_key='RESP_'+sender_id,body=resp)
+	resp['status_transfer'] = 1
+	resp = json.dumps(resp)
+	return channel.basic_publish(exchange='EX_TRANSFER',routing_key='RESP_'+sender_id,body=resp)
+
+
+def request_transfer(ch, method, properties, body):
+	try:
+		msg = json.loads(body.decode("utf-8"))
+		print ("[x] ",msg['action']," message :",msg)
+		transfer(msg)
+	except Exception as e"
+		print ("[E] Error :",e)
+
+channel.basic_consume(request_transfer, queue=queue_name, no_ack=True)
+
+channel.exchange_declare(exchange='EX_GET_SALDO', exchange_type='direct',durable=True)
+result = channel.queue_declare()
+queue_name = result.method.queue
+channel.queue_bind(exchange='EX_GET_SALDO',queue=queue_name,routing_key='REQ_1406559055')
+def get_saldo(msg):
+	database.connect()
+	resp = {}
+	resp['action'] = 'get_saldo'
+	resp['type'] = 'response'
+	resp['ts']= '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+	try :
+		user_id = msg['user_id']
+		sender_id = msg['sender_id']
+	except Exception as e:
+		print ("[E] Error :",e)
+		resp['nilai_saldo'] = -99
+		resp = json.dumps(resp)
+		return channel.basic_publish(exchange='EX_GET_SALDO',routing_key='RESP_'+sender_id,body=resp)
+	try:
+		user = User.get(npm=user_id)
+	except Exception as e:
+		print ("[E] Error :",e)
+		resp['nilai_saldo'] = -1
+		resp = json.dumps(resp)
+		return channel.basic_publish(exchange='EX_GET_SALDO',routing_key='RESP_'+sender_id,body=resp)
+	try:
+		reps['nilai_saldo'] = user.saldo
+	except Exception as e:
+			print ("[E] Error :",e)
+			resp['nilai_saldo'] = -4
+			resp = json.dumps(resp)
+			return channel.basic_publish(exchange='EX_GET_SALDO',routing_key='RESP_'+sender_id,body=resp)
+	resp = json.dumps(resp)
+	return channel.basic_publish(exchange='EX_TRANSFER',routing_key='RESP_'+sender_id,body=resp)
+
+
+def request_get_saldo(ch, method, properties, body):
+	try:
+		msg = json.loads(body.decode("utf-8"))
+		print ("[x] ",msg['action']," message :",msg)
+		get_saldo(msg)
+	except Exception as e"
+		print ("[E] Error :",e)
+
+channel.basic_consume(request_get_saldo, queue=queue_name, no_ack=True)
 
 channel.start_consuming()
--- p-ewallet.py
+++ p-ewallet.py
@@ -1,6 +1,10 @@
 # ewallet.py
 import pika, json, datetime, time
+from peewee import *
+from models import *
+from playhouse.sqlite_ext import SqliteExtDatabase
 
+database = SqliteExtDatabase('tugas2.db', journal_mode='WAL')
 params = pika.URLParameters('amqp://sisdis:sisdis@172.17.0.3:5672')
 connection = pika.BlockingConnection(params)
 channel = ''
@@ -30,16 +34,26 @@ def register(user_id,nama,req_id):
     msg['type'] = 'request'
     msg['ts']= '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     msg = json.dumps(msg)
-    result = channel.queue_declare(exclusive=True)
+    result = channel.queue_declare()
     queue_name = result.method.queue
     channel.queue_bind(exchange='EX_REGISTER',queue=queue_name,routing_key='RESP_1406559055')
     channel.basic_consume(response_register, queue=queue_name, no_ack=True)
     channel.basic_publish(exchange='EX_REGISTER',routing_key='REQ_'+req_id,body=msg)
-    print ("[x] published")
+    print ("[x] Register request published")
     print ("waiting response")
     channel.start_consuming()
 
-
+def response_saldo(ch, method, properties, body):
+        try :
+            msg = json.loads(body.decode("utf-8"))
+        except Exception as e:
+            print ("[E] Receive :",body)
+            print ("[E] Error :",e)
+        try :
+            print ("nilai_saldo = ",msg['nilai_saldo'])
+        except Exception as e:
+            print ("[E] Error :",e)
+        connection.close()
 
 def saldo(user_id,req_id):
     channel = connection.channel()
@@ -51,7 +65,59 @@ def saldo(user_id,req_id):
     msg['type'] = 'request'
     msg['ts']= '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     msg = json.dumps(msg)
+    result = channel.queue_declare()
+    queue_name = result.method.queue
+    channel.queue_bind(exchange='EX_GET_SALDO',queue=queue_name,routing_key='RESP_1406559055')
+    channel.basic_consume(response_transfer, queue=queue_name, no_ack=True)
     channel.basic_publish(exchange='EX_GET_SALDO',routing_key='REQ_'+req_id,body=msg)
+    channel.start_consuming()
+
+def simpan(user_id,nilai):
+    database.connect()
+    try:
+        user = User.get(npm=user_id)
+        user.saldo += nilai
+        print ("[B] Saldo anda ",user.saldo)
+        user.save()
+    except Exception as e:
+        print ("[E] Error :",e)
+    database.close()
+
+def ambil(user_id,nilai):
+    database.connect()
+    try:
+        user = User.get(npm=user_id)
+        user.saldo -= nilai
+        print ("[B] Saldo anda ",user.saldo)
+        user.save()
+    except Exception as e:
+        print ("[E] Error :",e)
+    database.close()
+
+def response_transfer(ch, method, properties, body):
+    try :
+        msg = json.loads(body.decode("utf-8"))
+    except Exception as e:
+        print ("[E] Receive :",body)
+        print ("[E] Error :",e)
+    try :
+        print ("status_transfer = ",msg['status_transfer'])
+    except Exception as e:
+        print ("[E] Error :",e)
+    if msg['status_transfer'] == 1 :
+        ambil(user_id,nilai)
+        print ("[X] Transfer berhasil! ")
+    elif msg[''status_transfer] == -2 :
+        print ("[E] Quorum tidak terpenuhi")
+    elif msg[''status_transfer] == -1 :
+        print ("[E] USER_ID tidak terdaftar, silahkan melakukan registrasi")
+    elif msg[''status_transfer] == -4 :
+        print ("[E] Terjadi kesalahan pada database")
+    elif msg[''status_transfer] == -5 :
+        print ("[E] Nilai transfer diluar ketentuan")
+    elif msg[''status_transfer] == -99 :
+        print ("[E] Terjadi kesalahan, belum terdefinisi")
+    connection.close()
 
 def transfer(user_id,nilai,req_id):
     channel = connection.channel()
@@ -64,7 +130,14 @@ def transfer(user_id,nilai,req_id):
     msg['type'] = 'request'
     msg['ts']= '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     msg = json.dumps(msg)
+    result = channel.queue_declare()
+    queue_name = result.method.queue
+    channel.queue_bind(exchange='EX_TRANSFER',queue=queue_name,routing_key='RESP_1406559055')
+    channel.basic_consume(response_transfer, queue=queue_name, no_ack=True)
     channel.basic_publish(exchange='EX_TRANSFER',routing_key='REQ_'+req_id,body=msg)
+    print ("[X] Transfer request published")
+    print ("waiting response")
+    channel.start_consuming()
 
 if (arg[0] == 'register'):
     user_id = arg[1]
@@ -80,3 +153,11 @@ elif (arg[0] == 'transfer'):
     nilai=arg[2]
     req_id=arg[3]
     transfer(user_id,nilai,req_id)
+elif (arg[0] == 'simpan'):
+    user_id=arg[1]
+    nilai = arg[2]
+    simpan(user_id,nilai)
+elif (arg[0] == 'ambil'):
+    user_id=arg[1]
+    nilai = arg[2]
+    ambil(user_id,nilai)
